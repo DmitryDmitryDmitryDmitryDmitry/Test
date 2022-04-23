4cea2c13e822b6cdea3cc481c6687fb03febc21f
DmitryDmitryDmitryDmitryDmitry <dubovskiy-1997@mail.ru>
2022-04-23 21:51:32 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -15,25 +15,25 @@ public class Main {
             @Override
             public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
                 super.startElement(uri, localName, qName, attributes);
-                String name = attributes.getValue("groupId");
+                String name = attributes.getValue("sex");
                 if (name != null && !name.isEmpty()){
                     System.out.println(name);
                 }
             }
 
-            @Override
+            /*@Override
             public void characters(char[] ch, int start, int length) throws SAXException {
                 String str = "";
                 for (int i=0; i<length;i++){
                     str+=ch[start+i];
                 }
-                System.out.println(str);
-            }
+                System.out.println();
+            } */
         };
 
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser = factory.newSAXParser();
-        parser.parse(new File("C:\\Users\\fluor\\IdeaProjects\\Mvn-testlib\\pom.xml"), handler);
+        parser.parse(new File("C:\\Users\\fluor\\IdeaProjects\\AddLibrary-test\\Struct.xml"), handler);
 
     }
 }
