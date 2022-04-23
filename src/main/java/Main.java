fe840c4150898c581e448a941d748247675b0102
DmitryDmitryDmitryDmitryDmitry <dubovskiy-1997@mail.ru>
2022-04-23 21:44:42 +0300

    GG

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        DefaultHandler handler = new DefaultHandler(){
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                super.startElement(uri, localName, qName, attributes);
+                String name = attributes.getValue("groupId");
+                if (name != null && !name.isEmpty()){
+                    System.out.println(name);
+                }
+            }
+
+            @Override
+            public void characters(char[] ch, int start, int length) throws SAXException {
+                String str = "";
+                for (int i=0; i<length;i++){
+                    str+=ch[start+i];
+                }
+                System.out.println(str);
+            }
+        };
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        parser.parse(new File("C:\\Users\\fluor\\IdeaProjects\\Mvn-testlib\\pom.xml"), handler);
+
+    }
+}
